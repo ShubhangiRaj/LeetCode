2d43793270538b29f3951c9606007915c46f788a
Shubhangi Raj Agrawal <shubhi.raj13@gmail.com>
2021-10-20 17:11:48 -0700

    adding intermediate commit

--- Easy/thirdMaxNumber.js
+++ Easy/thirdMaxNumber.js
@@ -2,15 +2,24 @@
 // If the third maximum does not exist, return the maximum number.
 
 var thirdMax = function(nums) {
-    if(nums.length < 3){
-        console.log(Math.max(...nums));
-        return Maths.max(...nums);
-    }
+    let maximum = new Set;
+     for(let num of nums){
+         maximum.add(num);
+         if(maximum.size > 3){
+             maximum.delete(Math.min(...maximum));
+         }
+     }
+     if(maximum.size === 3){
+         console.log(Math.min(...maximum))
+         return Math.min(...maximum);
+     } else {
+         console.log(Math.max(...maximum));
+         return Math.max(...maximum);
+     }
+ };
 
-};
+ thirdMax([1,2,2,5,3,5]);
+// thirdMax([2,2,3,1]);
+// thirdMax([3,2,1]);
+// thirdMax([1,2])
 
-thirdMax([2,2,3,1]);
-thirdMax([3,2,1]);
-thirdMax([1,2])
-
-// todo
