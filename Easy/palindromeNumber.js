e987a50debc6565e72bd0cb5137569350c490b1f
Shubhangi Raj Agrawal <shubhi.raj13@gmail.com>
2021-10-29 16:52:26 -0700

    adding intermediate commit

--- Easy/palindromeNumber.js
+++ Easy/palindromeNumber.js
@@ -4,6 +4,23 @@
 // 121 is palindrome while 123 is not.
 
 function isPalindrome(x){
+    if(x < 0 || (x % 10 === 0 && x !== 0)){
+        console.log(false);
+        return false;
+    }
+    let num = x;
+    let reverseNum = 0;
+    while(num > 0){
+        reverseNum = reverseNum * 10 + num % 10;
+        num = parseInt(num/10);
+    }
+    if(x === reverseNum){
+        console.log(true);
+        return true;
+    } else {
+        console.log(false);
+        return false;
+    }
 }
 
 isPalindrome(121);
@@ -11,4 +28,3 @@ isPalindrome(-121);
 isPalindrome(123);
 isPalindrome(100);
 
-// todo
