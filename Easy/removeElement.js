db8c8eb8427450faa6b48be7d07572adf5808572
Shubhangi Raj Agrawal <shubhi.raj13@gmail.com>
2021-11-11 16:27:15 -0800

    adding intermediate commit

--- Easy/removeElement.js
+++ Easy/removeElement.js
@@ -1,30 +1,29 @@
 var removeElement = function(nums, val){
-    for(let [index, value] of nums.entries()){
-        if(value == val){
-            nums[index] = "_";
-        }
-        if(value !== val && index > nums.indexOf("_")){
-           if(nums.indexOf("_") == -1){
-            continue;
-            } else{
-               nums[nums.indexOf("_")] = value;
-               nums[index] = "_";
-            }
+    let pointer1 = 0;
+    let pointer2 = nums.length - 1;
+    while(pointer1 < pointer2){
+        if((nums[pointer1] == val && nums[pointer2] !== val)){
+//             swap
+            [nums[pointer1], nums[pointer2]] = [nums[pointer2], nums[pointer1]];
+            pointer1++;
+            pointer2--;
+        } else if((nums[pointer1] !== val && nums[pointer2] == val)){
+            pointer1++;
+            pointer2--;
+        } else if(nums[pointer1] == val && nums[pointer2] == val){
+            pointer2--;
+        } else if(nums[pointer1] !== val && nums[pointer2] !== val){
+            pointer1++;
         }
     }
     console.log(nums);
-    let countOfRemainingElems = 0;
-    let i = 0;
-    while(i < nums.length){
-        if(nums[i] !== "_"){
-            countOfRemainingElems++;
-        }
-        i++;
-    }
-    return countOfRemainingElems, nums;
+    console.log(pointer1 + 1);
+    return pointer1 + 1;
 }
 
 // arr =[3,2,2,3], val = 3
 // Output => [2,2]
 
-removeElement([3,4,2,2,3,4,1,2], 3)
+removeElement([0,1,2,2,3,0,4,2], 2)
+
+// todo
